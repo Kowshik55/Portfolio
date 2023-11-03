f3933b5d7061e118ff4bde2e127507e38c723e24
Kowshik55 <ngummadi@unomaha.edu>
2023-11-03 13:13:09 -0500

    Footer changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,15 +16,15 @@ function Footer() {
     const { theme }  = useContext(ThemeContext)
 
     return (
-        <div className="footer" style={{backgroundColor: theme.secondary}}>
-            <p style={{color: theme.tertiary}}>
-                Made with  
-                <span style={{color: theme.primary, margin: '0 0.5rem -1rem 0.5rem'}}>
-                    ❤
-                </span>
-                 by {shortname(headerData.name)}
-            </p>
-        </div>
+        // <div className="footer" style={{backgroundColor: theme.secondary}}>
+        //     <p style={{color: theme.tertiary}}>
+        //         Made with  
+        //         <span style={{color: theme.primary, margin: '0 0.5rem -1rem 0.5rem'}}>
+        //             ❤
+        //         </span>
+        //          by {shortname(headerData.name)}
+        //     </p>
+        // </div>
     )
 }
 
